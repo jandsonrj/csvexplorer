f82125afcee93672061574397077bf9ee7f27269
Jandson Vitorino <jandson1512@gmail.com>
2023-10-06 01:32:35 -0300

    ok

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Testing React with Jest and Enzyme</h1>
+    </div>
+  );
+}
+
+export default App;
